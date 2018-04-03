7d473c324b836b1ee1f564876b95202723384ed7
Takahiro Miura <mc2.kage.super@gmail.com>
2018-04-03 21:39:37 +0900

    各spider作成、京大早稲田明治のtxt追加

---
+++ mayfes/spiders/aogaku.py
@@ -0,0 +1,31 @@
+import scrapy
+from .. import items
+
+class AogakuSpider(scrapy.Spider):
+    name = "aogaku"
+    allowed_domains = ["search.yahoo.co.jp"]
+    start_urls = [
+        "https://search.yahoo.co.jp/search?p=青山学院大学&dups=1&b={0}".format(x)
+        for x in range(1,1000,10)
+    ]
+
+    next_path1 = '//*[@id="pg"]/a[11]/@href'
+    next_path2 = '//*[@id="pg"]/a[10]/@href'
+    search_list_path = '//*[@id="web"]/ol/li'
+    titles_path = 'a/text()'
+    texts_path = 'div/text()'
+
+    def parse(self, response):
+        search_responses = response.xpath(AogakuSpider.search_list_path)
+        for search_response in search_responses:
+            item = items.MayfesItem()
+            item["title"] = [
+                box.strip() for box in search_response.xpath(AogakuSpider.titles_path).extract()]
+            item["texts"] = [
+                box.strip() for box in search_response.xpath(AogakuSpider.texts_path).extract()]
+            yield item
+
+        # next_page = response.xpath(AogakuSpider.next_path1).extract_first() or response.xpath(AogakuSpider.next_path2).extract_first()
+        # if next_page:
+        #     url = response.urljoin(next_page)
+        #     yield scrapy.Request(url, callback=self.parse)

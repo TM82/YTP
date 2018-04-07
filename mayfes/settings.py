57024b6c081412819b893f69ec8466dec0ae9dde
Takahiro Miura <mc2.kage.super@gmail.com>
2018-04-08 08:24:17 +0900

    東大と京大に固有のtxtデータ作成

--- mayfes/settings.py
+++ mayfes/settings.py
@@ -30,7 +30,7 @@ LOG_LEVEL = 'DEBUG'
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 120
+DOWNLOAD_DELAY = 90
 FEED_EXPORT_ENCODING='utf-8'
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16

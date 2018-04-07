5295473b22391b723472b23fd9017dd417d38286
Takahiro Miura <mc2.kage.super@gmail.com>
2018-04-08 07:54:31 +0900

    京都大学のjson作成完了

--- mayfes/almighty.sh
+++ mayfes/almighty.sh
@@ -1,9 +1,9 @@
 file_end_name=".json"
 file_start_name="./json_data"
-univ_name="東京大学" # TODO:大学名記入
-univ_dir="/todai/" # TODO:フォルダ名記入
+univ_name="京都大学" # TODO:大学名記入
+univ_dir="/kyodai/" # TODO:フォルダ名記入
 file_start_univ_name=$file_start_name$univ_dir
-for word in 研究 教授 サークル ベンチャー 留学生 病院 部活 大学院 技術 学部 国際 科学
+for word in タイムズ・ハイアー・エデュケーション 時計 特性 宇治 造形
 do
     filename=$file_start_univ_name$word$file_end_name
     scrapy crawl almighty -o $filename -a univ_name=$univ_name -a word=$word

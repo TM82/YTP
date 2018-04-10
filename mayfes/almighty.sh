deb0c2c77c93d6e632d9ed3061f18c5c90653f81
Takahiro Miura <mc2.kage.super@gmail.com>
2018-04-10 18:04:06 +0900

    早稲田のtxt追加

--- mayfes/almighty.sh
+++ mayfes/almighty.sh
@@ -1,7 +1,7 @@
 file_end_name=".json"
 file_start_name="./json_data"
-univ_name="東京大学" # TODO:大学名記入
-univ_dir="/todai/" # TODO:フォルダ名記入
+univ_name="早稲田大学" # TODO:大学名記入
+univ_dir="/waseda/" # TODO:フォルダ名記入
 file_start_univ_name=$file_start_name$univ_dir
 for word in 研究 教授 サークル ベンチャー 留学生 病院 部活 大学院 技術 学部 国際 科学
 do

b6fab0cee10b48a3cdb54f0551caaf26117107ff
Yukiko TAKEUCHI <yukiko@Akihito-MacBook-Air.local>
2018-04-15 12:43:58 +0900

    慶應に関連する語で検索した結果

--- mayfes/single.sh
+++ mayfes/single.sh
@@ -1,6 +1,6 @@
 file_end_name=".json"
 file_start_name="./json_data/1stLayer/" # TODO:フォルダ名記入
-for word in 研究 学生 キャンパス 情報 サイト 大学 大学院 入試 総合 消費 生活協同組合 発信 本郷 駒場 平成 教育 医療 社会 # TODO:検索したい単語を記入
+for word in 学部 月 医学部 紹介 マンション 内科 案内 病院 三田 教授 請求 日吉キャンパス # TODO:検索したい単語を記入
 do
     filename=$file_start_name$word$file_end_name
     scrapy crawl single -o $filename -a word=$word

71bbad97de983e0ecbb210b4a6bf76c51feafda0
Takahiro Miura <mc2.kage.super@gmail.com>
2018-04-18 09:58:54 +0900

    リンクを作るipynb作成

--- mayfes/query.sh
+++ mayfes/query.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file_end_name=".json"
-file_start_name="./json_data/" # TODO:フォルダ名記入
-for word in 請求 病院 # TODO:検索したい単語を記入
+file_start_name="./json_data/2ndLayer/" # TODO:フォルダ名記入
+for word in 国立 検索 オンライン サービス 利用 資料 コンテンツ 所蔵 申込 略称 申 デジタル 各種 蔵書 お知らせ ホームページ 予約 # TODO:検索したい単語を記入
 do
     filename=$file_start_name$word$file_end_name
     if [ ! -e $filename ]; then

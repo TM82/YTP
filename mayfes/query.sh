0f44784ce8dc8aab00575b7b9327e777c49f2c54
Takahiro Miura <mc2.kage.super@gmail.com>
2018-04-22 02:10:31 +0900

    コード、推進のjson取得

--- mayfes/query.sh
+++ mayfes/query.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file_end_name=".json"
 file_start_name="./json_data/2ndLayer/" # TODO:フォルダ名記入
-for word in 書 確認 au カタログ 料金 保険 方法 ページ 電話 支払い 証明 明細 手続き 金額 問い合わせ　# TODO:検索したい単語を記入
+for word in 公立 バー セキュリティ QR 数字 カメラ 操作 入力 使用 読み取り 画面 計画 支援 基本 葬儀 管理 行政 推進　# TODO:検索したい単語を記入
 do
     filename=$file_start_name$word$file_end_name
     if [ ! -e $filename ]; then

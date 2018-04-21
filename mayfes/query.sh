78708d02baf3c5d93fdcac413fcfdebaf0bfb2aa
Takahiro Miura <mc2.kage.super@gmail.com>
2018-04-22 02:30:49 +0900

    研究室のサーバで回した、物件、講座、民主、入試

--- mayfes/query.sh
+++ mayfes/query.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file_end_name=".json"
 file_start_name="./json_data/2ndLayer/" # TODO:フォルダ名記入
-for word in 公立 バー セキュリティ QR 数字 カメラ 操作 入力 使用 読み取り 画面 計画 支援 基本 葬儀 管理 行政 推進　# TODO:検索したい単語を記入
+for word in 相談 曜日 受付 窓口 － 休 所在地 通報 紛争 解決 トップページ 本文 位置 者 方 公開 ご覧 検討 保険 ID 国立 推進 # TODO:検索したい単語を記入
 do
     filename=$file_start_name$word$file_end_name
     if [ ! -e $filename ]; then

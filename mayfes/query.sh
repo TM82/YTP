142c17d0d6fb5dad93e3e5787885445d49fcc5bf
Takahiro Miura <mc2.kage.super@gmail.com>
2018-04-23 14:47:29 +0900

    駒場、サポートの取得

--- mayfes/query.sh
+++ mayfes/query.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file_end_name=".json"
 file_start_name="./json_data/2ndLayer/" # TODO:フォルダ名記入
-for word in 相談 曜日 受付 窓口 － 休 所在地 通報 紛争 解決 トップページ 本文 位置 者 方 公開 ご覧 検討 保険 ID 国立 推進 # TODO:検索したい単語を記入
+for word in 東大 目黒 周辺 フェリー 始発 終電 飛行機 電車 定期 製品 質問 ダウンロード お客様 修理 パスワード パソコン# TODO:検索したい単語を記入
 do
     filename=$file_start_name$word$file_end_name
     if [ ! -e $filename ]; then

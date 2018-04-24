05b2afc8e1ba1a0102e3f8ce17f5f4def8785c3c
Takahiro Miura <mc2.kage.super@gmail.com>
2018-04-24 13:57:58 +0900

    活動、案内のjson取得

--- mayfes/query.sh
+++ mayfes/query.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file_end_name=".json"
 file_start_name="./json_data/2ndLayer/" # TODO:フォルダ名記入
-for word in 東大 目黒 周辺 フェリー 始発 終電 飛行機 電車 定期 製品 質問 ダウンロード お客様 修理 パスワード パソコン# TODO:検索したい単語を記入
+for word in 交流 関西 成田空港 ターミナル フライト 駐車 航空 中国 注意 KB 改定 報酬 帝京大学 予算 # TODO:検索したい単語を記入
 do
     filename=$file_start_name$word$file_end_name
     if [ ! -e $filename ]; then
